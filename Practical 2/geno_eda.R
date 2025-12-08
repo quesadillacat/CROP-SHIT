9556fb61c7be10a6047e49d189ef79917aefd617
Sammi Johnson <sambjohnson94@protonmail.com>
2025-12-08 21:01:21 +0100

    add our 6 accession subset to readme, mv rproj, geno eda WIP

--- Practical 2/geno_eda.R
+++ Practical 2/geno_eda.R
@@ -1,15 +1,62 @@
 library(readxl)
 library(dplyr)
-
+library(tidyr)
+library(adegenet)
+library(hierfstat)
 
 kasp_dec3_raw <- read_excel("KASP shit/2025-12-03_081443.xlsx")
 kasp_dec4_raw <- read_excel("KASP shit/2025-12-04_084604.xlsx")
 kasp_mk130_dna <- read_excel("KASP shit/MK130_DNA_Nov2025_second_expierments_011_27_2025.xlsx")
 
-our.plants <- c("ASSYST-120", "ASSYST-145", "ASSYST-210", "ASSYST-271", "ASSYST-419", "ASSYST-424")
-
 pflzn <- data.frame(
-  accessions = c("ASSYST-120", "ASSYST-145", "ASSYST-210", "ASSYST-271", "ASSYST-419", "ASSYST-424"),
-  name       = c("Darmor"),
-  type       = c("WOSR")
-  , row.names = FALSE, stringsAsFactors = TRUE)
+  accessions = c("BnASSYST_120", "BnASSYST_145", "BnASSYST_210", "BnASSYST_271", "BnASSYST_419", "BnASSYST_424"),
+  name       = c("Darmor",       "Hokkai 3-Go",  "RED RUSSIAN",  "Liho",         "Angus",        "Conqueror Bronze Green Top"),
+  type       = c("Winter OSR",   "Winter OSR",   "Siber. Kale",  "Spring OSR",   "swede",        "swede")
+  )
+
+# Read in SNP chip data, transpose, give column names from 1st row and trim
+snp_raw <- read.table("MK130_60k_ASSYST_selected.txt", header = T)
+snp <- data.frame(t(snp_raw))
+colnames(snp) <- snp[1,]
+snp <- snp[-1,]
+
+
+# Set accessions as row IDs, extract those as our individuals
+# Extract crop types as populations
+# indivs <- as.character(rownames(snp))
+# populations <- as.character(snp$type)
+
+
+
+# to only grab our accessions
+snp_trim <- snp %>%
+  filter(row.names(snp) %in% pflzn$accessions)
+
+indivs <- as.character(rownames(snp_trim))
+populations <- as.character(snp$type)
+pflzn %>%
+  mutate(pflzn$type = populations)
+
+# trim off type (we'll use these extracted fields for genind instantiation)
+snp_2_genind <- snp_trim %>%
+  select(!type)
+
+
+
+
+
+
+
+
+
+
+
+###############################################################################
+# tidyverse re-write for my own readability reasons
+###############################################################################
+
+snp.ours <- snp_raw %>%
+  select(pflzn$accessions)
+
+indivs.ours <- pflzn$accessions
+populations.ours <- snp.ours
